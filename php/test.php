efab0150d3eaf6a30fd6fe577d14bb77efd6b270
Bo Sun <sunbocsg@gmail.com>
2014-01-19 16:01:54 +0800

    JSON

--- php/test.php
+++ php/test.php
@@ -8,12 +8,16 @@ if (mysqli_connect_errno())
  	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 else{
+//connection successfull
 	echo "success";
 	echo "<br>";
     $result = mysqli_query($con,"SELECT * FROM route");
     while($row =mysqli_fetch_array($result))
     {
-    	echo $row['id'] . " " . $row['name'] ." ". $row['type']."".$row['begin_time']." ".$row['end_time'];
+    	//echo $row['id'] . " " . $row['name'] ." ". $row['type']."".$row['begin_time']." ".$row['end_time'];
+    	$arr=array('name'=>$row['name'],'type'=>$row['type'],'begin_time'=>$row['begin_time'],'end_time'=>$row['end_time']);
+    	//echo $row['name'] ." ". $row['type']." ".$row['begin_time']." ".$row['end_time']
+    	echo json_encode(($arr));
     	echo "<br>"; }
     }
 ?>
