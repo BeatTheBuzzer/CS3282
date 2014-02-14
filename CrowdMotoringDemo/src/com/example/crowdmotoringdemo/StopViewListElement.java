1ae3ecbb264590452bafef6c58ab2c1372aa2116
Ivan Reinaldo <ivan.reinaldo92@gmail.com>
2014-02-14 15:21:47 +0800

    Implemented stop view list element

--- CrowdMotoringDemo/src/com/example/crowdmotoringdemo/StopViewListElement.java
+++ CrowdMotoringDemo/src/com/example/crowdmotoringdemo/StopViewListElement.java
@@ -1,5 +1,15 @@
 package com.example.crowdmotoringdemo;
 
 public class StopViewListElement {
+	public String busName;
+	public int arrivalTimeMin;
+	public String crowdedness;
+	public boolean toggleYes;
+	public boolean toggleNo;
+	
+	public StopViewListElement(){
+		
+	}
+	
 	
 }
